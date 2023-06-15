4ba8d7b5889c7bff757bf2812bcdc500c0f0b127
Percy <Jaberrwock123@163.com>
2023-06-15 13:14:46 +0800

    💡 V1.0 Modification suggestions.

--- IOSStore/AppDelegate.m
+++ IOSStore/AppDelegate.m
@@ -22,6 +22,23 @@
     
     self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
     
+    
+    //这里你使用的是系统自带的样式，UITabBarSystemItemFeatured、UITabBarSystemItemBookmarks这几个都是，但实际上真实项目里肯定都是用的自定义的样式，你可以参考以下代码试试看把这四个tabbar用自定义样式再实现一下
+    /*
+    vc.tabBarItem.title = 标题;
+    vc.tabBarItem.image = [UIImage imageNamed:@"显示的图片名"];//图片保存在Assets中
+    vc.tabBarItem.selectedImage = [UIImage imageNamed:@"选中情况下显示的图片名"];//上一句是未选中的情况下显示的图片，这里是选中情况下显示的图片
+    self.tabBar.tintColor=[UIColor colorNamed:@"Color_text"];//IOS13以后必须设置这个
+    
+    //这两句是调整图片和标题的间距
+    vc.tabBarItem.imageInsets = UIEdgeInsetsZero;
+    vc.tabBarItem.titlePositionAdjustment = UIOffsetMake(0, -2);
+    */
+    /*
+     除了上了写的，自定义tabBar还可以设置一些其他内容，具体的你可以按住command，然后点击tabBar.后面的任意一个参数，比方说titlePositionAdjustment，就可以跳转到oc源代码对应的页面，苹果对每一个方法或参数都有详细的注释，可以翻译一下
+     */
+    
+    
     TodayPageController *todayPage = [[TodayPageController alloc]init];
     //todayPage.view.backgroundColor = [UIColor redColor];
     todayPage.tabBarItem = [[UITabBarItem alloc]initWithTabBarSystemItem:UITabBarSystemItemFeatured tag:0];
--- IOSStore/SearchPageController.m
+++ IOSStore/SearchPageController.m
@@ -29,6 +29,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    /*
+     这里其实苹果商店页是整个一个UICollectionView包裹下的，你可以滑动一下苹果的商店页，他是整页，包括搜索框、标题栏都可以跟着一起滑动的，说明整个页面是一个整体，而不是仅仅下面列表那一部分是UITableView
+     
+     如果要实现苹果商店页的那种效果，就不能仅仅在cellForRowAtIndexPath里面写几句话就搞定了，要自定义至少三个UICollectionViewCell：最上方的搜索栏为一个单独cell，探索更多地下的六个搜索选项为一个cell，为你推荐底下的列表为一个cell，而那三个大标题（搜索、探索更多、为你推荐）可以公用一个UICollectionViewHeader，具体的实现代码量比较大，你可以自行百度
+     */
+    
     self.mySearchLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 80, 100, 40)];
     self.mySearchLabel.text = @"搜索";
     [self.mySearchLabel setFont:[UIFont systemFontOfSize:32]];
@@ -48,6 +54,11 @@
     [self.view addSubview:searchBar];
     searchBar.placeholder = @"游戏、APP、故事等";
     
+    
+    
+    /*
+     实际项目里最好不要这样用for循环的方式创建控件，因为这样会占用很多内存，有些项目对优化有要求。不过你现阶段这么写也没事，后面你有时间可以去了解一下UITableView或者UICOllectionView的嵌套，就是这个页面外面不是一个大的UICollectionView吗？这里你在这个UICollectionViewCell里面可以再嵌套一个小的UITabeView，这个UITabeView里面的每一个cell就是一个搜索选项，通过UITableView的复用可以减少资源的消耗。
+     */
     NSArray *searchTitleArray = @[@"塔防",@"绘本",@"经营类游戏",@"休闲游戏",@"三国策略",@"公主游戏"];
     for(int i=0; i<[searchTitleArray count]; i++){
         NSInteger index = i % 2;
@@ -61,6 +72,7 @@
     UITableView *tableView = [[UITableView alloc]initWithFrame:CGRectMake(20, 420, self.view.frame.size.width - 40, self.view.frame.size.height - 20) style:UITableViewStylePlain];
     tableView.delegate = self;
     tableView.dataSource = self;
+    //tableView.estimatedRowHeight = 65;//这句话是意思是动态设置cell高度，65只是一个预估值，实际上cell的高度是根据内容自动判断的
     [self.view addSubview:tableView];
      
 }
@@ -86,6 +98,7 @@
     return 5;
 }
 
+//这里你可以去看一下tableview的estimatedRowHeight这个属性（在上面的代码里面我写了用法），可以动态的设置cell高度，动态设置有利有弊，好处是方便，不用你自己算高度了，在复杂页面里面很好用，但弊端是有时候会出现闪屏，注意：如果你设置了tableView.estimatedRowHeight了，那么下面这段heightForRowAtIndexPath就可以注释掉了，两者不要同时用
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return 65;
 }
@@ -93,7 +106,11 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     // Configure the cell...
     NSString *cellID = @"cellID";
+    
+    //这里你可以去了解一下UITableView的复用机制，就是下面这句话为什么要设置cellID
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    
+    
     if (cell == nil) {
         cell = [[RecommendViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
         // 去除分割线
@@ -102,6 +119,12 @@
         //cell.selectionStyle = UITableViewCellSelectionStyleBlue;
         
     }
+    
+    /*
+     UITableViewCell是系统默认的cell方式，但实际上项目里面一般都是自定义cell的，你可以百度一下如何自定义cell
+     */
+    
+    
     
     return cell;
 }
